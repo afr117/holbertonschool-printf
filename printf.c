d2b25fa1b5262c821d7670c0bb7fb3ac6d277be2
afr117 <alfredo.figueroa07@yahoo.com>
2023-11-08 19:42:07 -0800

    test task1

--- printf.c
+++ printf.c
@@ -1,72 +1,101 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
-
 /**
- * _printf - Custom printf function
- * @format: Format string
- * Return: Number of characters printed
- */
+ * _printf - main function to print in console
+ * @format: array to print and check type
+ * Return: count of character printed
+ **/
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    const char *ptr;
-    char c;
-    char *str;
+int count = 0;
+va_list args;
+if (format == NULL)
+	{
+	return (-1);
+	}
+	va_start(args, format);
+	while (*format)
+	{
+	if (*format == '%')
+	{
+		format++;
+		switch (*format)
+	{
+		case 'c':
+		{
+			char c = va_arg(args, int);
 
-    va_start(args, format);
-    ptr = format;
+			count += printf("%c", c);
+			break;
+		}
+		case 's':
+		{
+			char *s = va_arg(args, char *);
 
-    while (ptr && *ptr)
-    {
-        if (*ptr == '%')
-        {
-            ptr++;
-            switch (*ptr)
-            {
-                case 'c':
-                    c = va_arg(args, int);
-                    write(1, &c, 1);
-                    count++;
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (str)
-                    {
-                        while (*str)
-                        {
-                            write(1, str, 1);
-                            str++;
-                            count++;
-                        }
-                    }
-                    else
-                    {
-                        write(1, "(null)", 6);
-                        count += 6;
-                    }
-                    break;
-                case '%':
-                    write(1, "%", 1);
-                    count++;
-                    break;
-                default:
-                    write(1, "%", 1);
-                    write(1, ptr, 1);
-                    count += 2;
-                    break;
-            }
-        }
-        else
-        {
-            write(1, ptr, 1);
-            count++;
-        }
-        ptr++;
-    }
+			count += printf("%s", s);
+			break;
+		}
+		case '%':
+		{
+			count += printf("%%", *format);
+			break;
+		}
+		case 'd':
+		case 'i':
+		{
+			int num = va_arg(args, int);
 
-    va_end(args);
-    return count;
+			count += printf("%d", num);
+			break;
+		}
+		case 'u':
+		{
+			unsigned int num = va_arg(args, unsigned int);
+
+			count += printf("%u", num);
+			break;
+		}
+		case 'o':
+		{
+			unsigned int num = va_arg(args, unsigned int);
+
+			count += printf("%o", num);
+			break;
+		}
+		case 'x':
+		{
+			unsigned int num = va_arg(args, unsigned int);
+
+			count += printf("%x", num);
+			break;
+		}
+		case 'X':
+		{
+			unsigned int num = va_arg(args, unsigned int);
+
+			count += printf("%X", num);
+			break;
+		}
+		case 'p':
+		{
+			void *ptr = va_arg(args, void *);
+
+			count += printf("%p", ptr);
+			break;
+		}
+		default:
+			{
+			count += printf("%%%c", *format);
+			break;
+			}
+		}
+	}
+		else
+		{
+			count += printf("%c", *format);
+		}
+		format++;
+		}
+	va_end(args);
+	return count;
 }
-
