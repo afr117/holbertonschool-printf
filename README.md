25c4f4057e65a19b29e44bca3e66ff85b20d7a0f
afr117 <alfredo.figueroa07@yahoo.com>
2023-11-08 18:41:08 -0800

    ./printf correct out

---
+++ README.md
@@ -0,0 +1,11 @@
+# Holberton School Printf Project
+
+This project implements a custom printf function as per the task requirements.
+
+## Compilation
+
+To compile the program, use the following command:
+
+```sh
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -Wno-format *.c -o printf
+
---
+++ printf.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+    const char *ptr;
+    char c;
+    char *str;
+
+    va_start(args, format);
+    ptr = format;
+
+    while (*ptr)
+    {
+        if (*ptr == '%')
+        {
+            ptr++;
+            switch (*ptr)
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    write(1, &c, 1);
+                    count++;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    while (*str)
+                    {
+                        write(1, str, 1);
+                        str++;
+                        count++;
+                    }
+                    break;
+                case '%':
+                    write(1, "%", 1);
+                    count++;
+                    break;
+                default:
+                    write(1, "%", 1);
+                    write(1, ptr, 1);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            write(1, ptr, 1);
+            count++;
+        }
+        ptr++;
+    }
+
+    va_end(args);
+    return count;
+}
+
